30c404dfabca59771c3abbc29cff237e161b1282
Necdet Can Ateşman <can@strg.at>
2017-04-26 17:32:11 +0200

    New: Add two new unit tests, one failing
    Looks like global variables are not working yet.

--- test/rendering.py
+++ test/rendering.py
@@ -49,3 +49,29 @@ def test_string_rendering():
     assert score.tpl.render('foo.jinja2.tpl', {'data': 'foo'}) == 'foo'
     loader.load.assert_called_once_with('foo.jinja2.tpl')
     assert score.tpl.render('foo.jinja2.tpl', {'data': '<'}) == '&lt;'
+
+
+def test_function():
+    score = init_score(finalize=False)
+    loader = unittest.mock.Mock()
+    loader.load.return_value = (False, '{{ func() }}')
+    score.tpl.loaders['tpl'].append(loader)
+    score.tpl.filetypes['text/plain'].extensions.append('tpl')
+    score._finalize()
+    loader.load.assert_not_called()
+    assert score.tpl.render('foo.jinja2.tpl', {'func': lambda: 'foo'}) == 'foo'
+    loader.load.assert_called_once_with('foo.jinja2.tpl')
+    assert score.tpl.render('foo.jinja2.tpl', {'func': lambda: '<'}) == '&lt;'
+
+
+def test_global_function():
+    score = init_score(finalize=False)
+    loader = unittest.mock.Mock()
+    loader.load.return_value = (False, '{{ func() }}')
+    score.tpl.loaders['tpl'].append(loader)
+    score.tpl.filetypes['text/plain'].extensions.append('tpl')
+    score.tpl.filetypes['text/plain'].add_global('func', lambda: 'foo!')
+    score._finalize()
+    loader.load.assert_not_called()
+    assert score.tpl.render('foo.jinja2.tpl') == 'foo'
+    loader.load.assert_called_once_with('foo.jinja2.tpl')
