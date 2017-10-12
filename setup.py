865ada5a83eeb06ba17841c09e52cb20d1ecca9e
Necdet Can Ateşman <can@atesman.at>
2017-10-12 14:51:44 +0200

    Release: 0.0.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.jinja2',
-    version='0.0.5',
+    version='0.0.6',
     long_description=README,
     author='strg.at',
     author_email='score@strg.at',
